6a56c5c657757f8d72268ac6edc13944c22b5464
daus <dausg878@gmail.com>
2024-01-03 19:59:29 +0700

    last

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
 document.querySelector(".buka-undangan").addEventListener("click", () => {
   document.querySelector(".wrapper").classList.toggle("cover-active");
+  var audio = document.getElementById('myAudio');
+  audio.play();
 });

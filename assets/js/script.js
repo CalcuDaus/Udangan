4a93a23102d453adbc3e61e4881bbc842ad6a1fe
daus <dausg878@gmail.com>
2024-01-07 17:41:16 +0700

    add function copied

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,17 @@ document.querySelector(".buka-undangan").addEventListener("click", () => {
   var audio = document.getElementById('myAudio');
   audio.play();
 });
+function copyToClipboard(text) {
+  navigator.clipboard.writeText(text)
+    .then(() => {
+      alert('Teks Berhasil Disalin !');
+    })
+    .catch(err => {
+      console.error('Unable to copy to clipboard', err);
+    });
+}
+document.querySelector('#salin').addEventListener('click',function(){
+  let value = document.querySelector('.teks-copy').innerText;
+  this.innerText = 'copied';
+  copyToClipboard(value);
+})
